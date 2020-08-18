647e5454d7ead4f5853294c2b417068b1fbcab1e
HassanKhan123 <hk2602809@gmail.com>
2020-08-18 22:18:59 +0500

    getting a place by userId

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -26,6 +26,12 @@ router.get("/:pid", (req, res, next) => {
   res.json({ place });
 });
 
+router.get("/user/:uid", (req, res, next) => {
+    const userId = req.params.uid;
+    const place = DUMMY_PLACES.find((p) => p.creator === userId);
+    res.json({ place });
+  });
+
 // router.post('/',(req,res,next) => {
 //     console.log('POST request in places')
 //     res.json({message:'It works'})
