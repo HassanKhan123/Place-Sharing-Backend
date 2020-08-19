27ffef302250670ee071ccd5df2aea7df9ebb30f
HassanKhan123 <hk2602809@gmail.com>
2020-08-19 07:12:17 +0500

    handling errors for unsupported routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const HttpError = require('./models/http-error')
 const placesRoutes = require("./routes/places-routes");
 
 const app = express();
@@ -7,6 +8,11 @@ const app = express();
 app.use(express.json())
 
 app.use("/api/places/", placesRoutes);
+
+app.use((req,res,next) => {
+  const error = new HttpError('Could not find this route',404)
+  throw error
+})
 
 app.use((err, req, res, next) => {
   if (req.headerSent) {
