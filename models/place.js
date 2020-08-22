fd190435ccc88e28727725504802e54bf4f34e31
HassanKhan123 <hk2602809@gmail.com>
2020-08-22 17:23:56 +0500

    relation added between places and users

--- models/place.js
+++ models/place.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const PlaceSchema = new mongoose.Schema({
-    title: {type:String,required:true},
-    description: {type:String,required:true},
-    image: {type:String,required:true},
-    address: {type:String,required:true},
-    location: {
-      lat: {type:Number,required:true},
-      lng: {type:Number,required:true},
-    },
-    creator: {type:String,required:true}, 
-})
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+});
 
-module.exports = mongoose.model('Place',PlaceSchema)
+module.exports = mongoose.model("Place", PlaceSchema);
