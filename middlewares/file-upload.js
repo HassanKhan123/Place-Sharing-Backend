642ce8612032a1f5c56196ea8f974d0a869b6ac8
HassanKhan123 <hk2602809@gmail.com>
2020-08-26 19:54:32 +0500

    filtering images

--- middlewares/file-upload.js
+++ middlewares/file-upload.js
@@ -11,13 +11,19 @@ const fileUpload = multer({
   limits: 500000,
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null,'uploads/images')
+      cb(null, "uploads/images");
     },
     filename: (req, file, cb) => {
       const ext = MIME_TYPE_MAP[file.mimetype];
       cb(null, uuid.v4() + "." + ext);
     },
   }),
+  fileFilter: (req, file, cb) => {
+    const isValid = !!MIME_TYPE_MAP[file.mimetype];
+    let error = isValid ? null : new Error("Invalid mime type");
+
+    cb(error, isValid);
+  },
 });
 
 module.exports = fileUpload;
