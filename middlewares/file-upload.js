2383eca4190ca1ac781fdaf352d3e349e343bbed
HassanKhan123 <hk2602809@gmail.com>
2020-08-26 19:46:37 +0500

    using multer to save files

---
+++ middlewares/file-upload.js
@@ -0,0 +1,23 @@
+const multer = require("multer");
+const uuid = require("uuid");
+
+const MIME_TYPE_MAP = {
+  "image/png": "png",
+  "image/jpeg": "jpeg",
+  "image/jpg": "jpg",
+};
+
+const fileUpload = multer({
+  limits: 500000,
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null,'uploads/images')
+    },
+    filename: (req, file, cb) => {
+      const ext = MIME_TYPE_MAP[file.mimetype];
+      cb(null, uuid.v4() + "." + ext);
+    },
+  }),
+});
+
+module.exports = fileUpload;
--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const { check } = require("express-validator");
 
-
-const {getUsers,login,signup} = require('../controllers/users-controller')
+const { getUsers, login, signup } = require("../controllers/users-controller");
+const fileUpload = require("../middlewares/file-upload");
 
 const router = express.Router();
 
 router.get("/", getUsers);
 
-
-
-router.post("/signup",[
-    check('name').not().isEmpty(),
-    check('email').normalizeEmail().isEmail(),
-    check('password').isLength({min:6})
-], signup);
+router.post(
+  "/signup",
+  fileUpload.single("image"),
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  signup
+);
 router.post("/login", login);
-
-
-
-
 
 module.exports = router;
