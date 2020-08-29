296488a155c65dc615a814d9f82e03be4a402c7f
HassanKhan123 <hk2602809@gmail.com>
2020-08-29 11:16:57 +0500

    routes protection with auth middleware

---
+++ middlewares/check-auth.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      throw new Error("Authentication failed");
+    }
+    const decodedToken = jwt.verify(token, "supersecret");
+    req.userData = { userId: decodedToken.userId };
+    next();
+  } catch (error) {
+    return next(new HttpError("Authentication failed", 401));
+  }
+};
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -9,12 +9,15 @@ const {
   deletePlaceById,
 } = require("../controllers/places-controller");
 const fileUpload = require("../middlewares/file-upload");
+const checkAuth = require('../middlewares/check-auth')
 
 const router = express.Router();
 
 router.get("/:pid", getPlaceById);
 
 router.get("/user/:uid", getPlacesByUserId);
+
+router.use(checkAuth)
 
 router.post(
   "/",
